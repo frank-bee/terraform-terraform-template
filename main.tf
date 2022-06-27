a4e951bc39e57ec3caf40521feacedae925eea25
Frank Bernhardt <275327+frank-bee@users.noreply.github.com>
2022-06-27 07:15:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,3 @@ module "tags" {
   names        = [var.name]
   tags         = var.tags
 }
-
-locals {
-  # tflint-ignore: terraform_unused_declarations
-  name = module.tags.name
-  # tflint-ignore: terraform_unused_declarations
-  tags = module.tags.tags_no_name
-}
